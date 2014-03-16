d03eaa32bb17ee78e09776cf7d2c3a850e0d909e
Andrea Dixon <andrea.coa.dixon@gmail.com>
2014-03-16 09:07:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaveVisionMarketing::Application.config.secret_token = '9eb3dcfb06bc5da0359bc0ee680f2d46528e5f8624eef22acffca857e6de8526171355755dbc2ef45c83fad118f01a99172b3b9de11b9409cac926e9c7fc4545'
