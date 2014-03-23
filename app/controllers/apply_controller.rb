d35590cf8a171166a22089a4119764faf9017646
Andrea Dixon <andrea.coa.dixon@gmail.com>
2014-03-23 19:34:13 -0400

    mail try 3

--- app/controllers/apply_controller.rb
+++ app/controllers/apply_controller.rb
@@ -9,7 +9,7 @@ class ApplyController < ApplicationController
     	@company = Company.find_by_name("Wave Vision Marketing")
     
     	if @message.valid?
-      		NotificationsMailer.new_message(@message).deliver
+      		NotificationsMailer.new_message(@message)
       		redirect_to(root_path, :notice => "Message was successfully sent.")
     	else
       		flash.now.alert = "Please fill out all fields."
