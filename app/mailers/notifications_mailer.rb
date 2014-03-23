c83488bc100c5cd1061b9ccd92fdfae06599aa1c
Andrea Dixon <andrea.coa.dixon@gmail.com>
2014-03-23 18:38:54 -0400

    email try #1

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,11 +1,11 @@
 class NotificationsMailer < ActionMailer::Base
   default :from => "admin@wavevisionmarketing.com"
-  default :to => "admin@wavevisionmarketing"
+  default :to => "admin@wavevisionmarketing.com"
 
   def new_message(message)
     @message = message
     @company = Company.find_by_name("Wave Vision Marketing")
-    
+
     mail(:subject => "Questions" + @company.name)
   end
 end
