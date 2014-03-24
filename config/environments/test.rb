28d586cb9218c76b8eda1b8bd283b16756558bd6
Andrea Dixon <andrea.coa.dixon@gmail.com>
2014-03-23 22:47:39 -0400

    mail 9

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,4 +35,5 @@ WaveVisionMarketing::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
+  config.action_mailer.simple_postmark_settings = { api_key: 'POSTMARK_API_TEST', return_response: true }
 end
